3ce78faa3a9a9e9d2d147a91eee1ec8fc796a55a
Nora Söderlund <nora.soderlund@polestar.com>
2023-05-07 12:57:21 +0200

    Updated example function

--- src/functions/[example].ts
+++ src/functions/[example].ts
@@ -1,5 +1,5 @@
 export const onRequest: PagesFunction<Env> = async (context) => {
     const { example } = context.params;
 
-    return context.env.ASSETS.fetch(`/${example}/index.html?key=${context.env.GOOGLE_MAPS_PUBLIC_API}`);
+    return Response.redirect(`/${example}/?key=${context.env.GOOGLE_MAPS_PUBLIC_API}`);
 };
